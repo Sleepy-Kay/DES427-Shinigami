d5403224e2337333770980e4539e500a8466fdf9
VivackChan <923553199@sfsu.edu>
2025-05-19 05:39:02 -0700

    V

---
+++ 427 - Shinigami/Assets/playAnimation4.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playAnimation4 : MonoBehaviour
+{
+
+    public GameObject myCube;
+    public AudioSource Jumpscare_SoundEffect;
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = myCube.GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            anim.Play("Character3_Animation2", 0, 0.0f);
+            Jumpscare_SoundEffect.Play();
+        }
+    }
+}
